ba39fb3db1c2632f0623d3393763f399e2646b62
Virginia Ladd <virginialadd2@gmail.com>
2020-07-01 12:43:38 -0600

    Updated Login with a new button for Maze and Posts

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Login.css';
+import Posts from '../Posts/Posts';
 import PropTypes from 'prop-types';
 
 export const Login = (props) => {
   return (
     <div className='login'>
-      <form className='login-form'>
-        <h1>Welcome To My Studio Ghibli Fan Page</h1>
-        <input type='text' placeholder='Account Name' />
-      </form>
+      <div className='home-block'>
+        <form className='login-form'>
+          <h1>Welcome To My Studio Ghibli Fan Page</h1>
+          <button>Make A Cat</button>
+          <button>Explore Movies</button>
+          <button>Escape The Maze</button>
+        </form>
+      </div>
+      <div className='home-block'>
+        <Posts />
+      </div>
     </div>
   )
 }
