0bec35759a590c984e4baf68e8617ee900b01b06
Virginia Ladd <virginialadd2@gmail.com>
2020-07-06 14:13:55 -0600

    Create a mock user post for didplay

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from '../Login/Login';
+import Posts from '../Posts/Posts';
 
 class App extends Component {
   constructor() {
@@ -10,13 +11,16 @@ class App extends Component {
         id: 1,
         name: null
       },
-      favorites: []
+      posts: [
+        {id: 2, name: 'BigFan', content: 'I am the biggest Totoro fan around. I even have a Chibi tattoo on my arm. Love it all.'}
+      ]
     }
   }
   render () {
     return (
       <div className="App">
         < Login />
+        < Posts eachPost={this.state.posts} />
       </div>
     )
   }
