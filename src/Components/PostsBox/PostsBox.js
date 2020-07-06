508b74c1cd936023e39bd7c07bc9b540b3d90132
Virginia Ladd <virginialadd2@gmail.com>
2020-07-06 14:27:34 -0600

    Create post container component

---
+++ src/Components/PostsBox/PostsBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PostsBox.css';
+import Posts from '../Posts/Posts';
+
+const PostsBox = (props) => {
+  let allPosts = props.posts.map(post => <Posts id={post.id} key={post.id} name={post.name} content={post.content} />)
+  return (
+    <div className='post-block'>
+      {allPosts}
+    </div>
+  )
+}
+
+export default PostsBox;
