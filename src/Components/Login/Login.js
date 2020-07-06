8cc261d9549030014ba752bd9029b41947ae9345
Virginia Ladd <virginialadd2@gmail.com>
2020-07-06 14:42:47 -0600

    Added some inputs and a button for creation

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -13,6 +13,13 @@ export const Login = (props) => {
           <button>Explore Movies</button>
           <button>Escape The Maze</button>
         </form>
+        <form>
+          <h2>Say Something About Yourself</h2>
+          <input type='text' placeholder='Choose A Name' />
+          <input type='text' placeholder='Tell Us Your Favorite Ghbili Character' />
+          <input type='text' placeholder='Now Write Anything You Want!' />
+          <button type='submit' />
+        </form>
       </div>
       <div>
         <PostsBox posts={props.posts}/>
