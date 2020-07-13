1a9d6d6af45e3579f97a77f985960e28d4b13d28
Virginia Ladd <virginialadd2@gmail.com>
2020-07-13 11:14:44 -0600

    Add favorite movie to list of elements in posting

--- src/Components/PostsBox/PostsBox.js
+++ src/Components/PostsBox/PostsBox.js
@@ -3,7 +3,7 @@ import './PostsBox.css';
 import Posts from '../Posts/Posts';
 
 const PostsBox = (props) => {
-  let allPosts = props.posts.map(post => <Posts id={post.id} key={post.id} name={post.name} content={post.content} />)
+  let allPosts = props.posts.map(post => <Posts id={post.id} key={post.id} name={post.name} movie={post.movie} content={post.content} />)
   return (
     <div className='post-block'>
       {allPosts}
