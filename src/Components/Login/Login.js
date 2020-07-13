71dc92d1bd6c7b43ca385b4bc22e1ea74a95fde4
Virginia Ladd <virginialadd2@gmail.com>
2020-07-13 11:15:01 -0600

    Convert login component from function to class component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Login.css';
 import PostsBox from '../PostsBox/PostsBox';
 import PropTypes from 'prop-types';
 
-export const Login = (props) => {
-  return (
-    <div className='login'>
-      <div className='home-block'>
-        <form className='login-form'>
-          <h1>Welcome To My Studio Ghibli Fan Page</h1>
-          <button>Make A Cat</button>
-          <button>Explore Movies</button>
-          <button>Escape The Maze</button>
-        </form>
-        <form>
-          <h2>Say Something About Yourself</h2>
-          <input type='text' placeholder='Choose A Name' />
-          <input type='text' placeholder='Tell Us Your Favorite Ghbili Character' />
-          <input type='text' placeholder='Now Write Anything You Want!' />
-          <button type='submit' />
-        </form>
+class Login extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+
+    }
+  }
+  submitPost = (posting) => {
+
+  }
+  render() {
+    return (
+      <div className='login'>
+        <div className='home-block'>
+          <form className='login-form'>
+            <h1>Welcome To My Studio Ghibli Fan Page</h1>
+            <button>Make A Cat</button>
+            <button>Explore Movies</button>
+            <button>Escape The Maze</button>
+          </form>
+          <h3>or Add to Community Posts</h3>
+          <form>
+            <h2>Say Something About Yourself</h2>
+            <input type='text' placeholder='Choose A Name' />
+            <input type='text' placeholder='Tell Us Your Favorite Ghbili Character' />
+            <input type='text' placeholder='Now Write Anything You Want!' />
+            <button type='submit' onClick={this.submitPost}/>
+          </form>
+        </div>
+        <div>
+          <PostsBox posts={this.props.posts} />
+        </div>
       </div>
-      <div>
-        <PostsBox posts={props.posts}/>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Login.propTypes = {
