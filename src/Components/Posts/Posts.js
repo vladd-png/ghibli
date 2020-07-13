cfa38676e9787c3f9e33e1c97bf563777cd3e76c
Virginia Ladd <virginialadd2@gmail.com>
2020-07-13 13:23:57 -0600

    Stylized post block to be seperate blocks

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Posts.css';
 
 const Posts = (props) => {
   return (
-    <div>
+    <div className='single-post'>
       <h1>{props.name}</h1>
       <h3>{props.character}</h3>
       <p>{props.content}</p>
