b23eec553bde80fe29b9a9808475bcddd5b4c9d8
Virginia Ladd <virginialadd2@gmail.com>
2020-07-18 12:23:48 -0600

    Add a function to create random emojis around username

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Posts.css';
 
-const Posts = (props) => {
-  return (
-    <div className='single-post'>
-      <h1>{props.name}</h1>
-      <h3>{props.character}</h3>
-      <p>{props.content}</p>
-    </div>
-  )
+class Posts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  addEmoji() {
+    let allEmojis = ['✨', '🍬', '🌟', '🦄', '🦋', '🥳', '👽', '🍕', '🧩', '💎', '🔮', '💜'];
+    let randomEmoji = allEmojis[Math.floor(Math.random() * allEmojis.length)];
+    console.log(randomEmoji)
+    return `${randomEmoji} ${this.props.name} ${randomEmoji}`;
+  }
+  render () {
+    return (
+      <div className='single-post'>
+        <h1>{this.addEmoji()}</h1>
+        <h3>{this.props.character}</h3>
+        <p>{this.props.content}</p>
+      </div>
+    )
+  }
 }
 
 export default Posts;
