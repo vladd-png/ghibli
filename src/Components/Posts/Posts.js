68679c96bcf92b1f258dc8d76d0f7edcd68933b6
Virginia Ladd <virginialadd2@gmail.com>
2020-07-18 12:28:53 -0600

    Fix posting error with emojis

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -4,19 +4,21 @@ import './Posts.css';
 class Posts extends Component {
   constructor(props) {
     super(props)
+    this.newName = this.addEmoji();
     this.state = {
     }
   }
   addEmoji() {
     let allEmojis = ['✨', '🍬', '🌟', '🦄', '🦋', '🥳', '👽', '🍕', '🧩', '💎', '🔮', '💜'];
     let randomEmoji = allEmojis[Math.floor(Math.random() * allEmojis.length)];
-    console.log(randomEmoji)
-    return `${randomEmoji} ${this.props.name} ${randomEmoji}`;
+    this.newName = `${randomEmoji} ${this.props.name} ${randomEmoji}`;
+    return this.newName
   }
+  // small bug - runs every time not just when the post is created
   render () {
     return (
       <div className='single-post'>
-        <h1>{this.addEmoji()}</h1>
+        <h1>{this.newName}</h1>
         <h3>{this.props.character}</h3>
         <p>{this.props.content}</p>
       </div>
